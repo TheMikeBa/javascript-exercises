149403eb9e8935bb30368d275ec2661008799139
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-03-18 17:20:43 -0400

    COMPLETED 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (group) {
+  //   group.sort((a, b) => {
+  //     return b.yearOfDeath - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth);
+  //   });
 
+  //   group.forEach((person) =>
+  //     person.yearOfDeath && person.yearOfBirth
+  //       ? (person.age = person.yearOfDeath - person.yearOfBirth)
+  //       : (person.age = null)
+  //   );
+
+  group.forEach((person) =>
+    person.yearOfBirth
+      ? person.yearOfDeath
+        ? (person.age = person.yearOfDeath - person.yearOfBirth)
+        : (person.age = new Date().getFullYear() - person.yearOfBirth)
+      : null
+  );
+
+  group.sort((a, b) => b.age - a.age);
+
+  return group[0];
 };
 
 // Do not edit below this line
