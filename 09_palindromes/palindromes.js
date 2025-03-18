64ff4e8c8fe67b04a65b2257b60f1564e37e8b84
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-03-18 15:48:52 -0400

    COMPLETED palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  word = word.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const drow = word
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split("")
+    .reverse()
+    .join("");
+  return word === drow;
 };
 
 // Do not edit below this line
